4fb2bb1cd5655fee5364d98f54a15c334a640042
Celeste Bareiro <cnyx7@proton.me>
2023-03-14 01:37:11 +0000

    first commit

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,20 +1,38 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Context } from "../store/appContext";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
 export const Home = () => {
-	const { store, actions } = useContext(Context);
-
-	useEffect(() => { }, [store.usuario])
-
+	const [inputValue, setInputValue] = useState("");
+	const [toDos, settoDos] = useState([]);
 	return (
-		<div className="text-center mt-5">
-			<h1>{store.usuario.msg}</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<button type="button" onClick={() => { actions.login() }}>Login</button>
-		</div>
+		<>
+			<div className="container-fluid row d-flex justify-content-center align-items-center p-2">
+				<h1 className="row justify-content-center">To-Do List</h1>
+				<ul className="list-container col-12">
+					<li className="list-title">
+						<input
+							className="text"
+							onChange={(e) => setInputValue(e.target.value)}
+							value={inputValue}
+							onKeyPress={(e) => {
+								if (e.key === "Enter") {
+									settoDos(toDos.concat([inputValue]));
+									setInputValue("");
+								}
+							}}
+							placeholder="What do you need to do today?">
+						</input>
+					</li>
+					{toDos.map((item, index) => (
+						<li className="list-item">{item}
+							<button className="ocultar btn btn-sm btn-secondary" type="button" onClick={() => settoDos(toDos.filter((item, currentIndex) => index != currentIndex))}><i className="fas fa-trash-alt"></i></button>
+						</li>
+					))}
+				</ul>
+				<p><em>{toDos.length} item(s) left</em></p>
+			</div>
+		</>
 	);
 };
